cad9445b5a586cd28b8853272663b8ab38a04e32
Nic Cope <negz@rk0n.org>
2021-04-29 05:32:47 +0000

    Use a lowercase kind cluster name
    This appears to be a new restriction introduced since kind v0.7.0?
    
    Signed-off-by: Nic Cope <negz@rk0n.org>

--- cluster/local/integration_tests.sh
+++ cluster/local/integration_tests.sh
@@ -7,53 +7,55 @@ YLW='\033[0;33m'
 GRN='\033[0;32m'
 RED='\033[0;31m'
 NOC='\033[0m' # No Color
-echo_info(){
+echo_info() {
     printf "\n${BLU}%s${NOC}" "$1"
 }
-echo_step(){
+echo_step() {
     printf "\n${BLU}>>>>>>> %s${NOC}\n" "$1"
 }
-echo_sub_step(){
+echo_sub_step() {
     printf "\n${BLU}>>> %s${NOC}\n" "$1"
 }
 
-echo_step_completed(){
+echo_step_completed() {
     printf "${GRN} [✔]${NOC}"
 }
 
-echo_success(){
+echo_success() {
     printf "\n${GRN}%s${NOC}\n" "$1"
 }
-echo_warn(){
+echo_warn() {
     printf "\n${YLW}%s${NOC}" "$1"
 }
-echo_error(){
+echo_error() {
     printf "\n${RED}%s${NOC}" "$1"
     exit 1
 }
 
 # k8s watchers
 
-wait_for_pods_in_namespace(){
+wait_for_pods_in_namespace() {
     local timeout=$1
     shift
     namespace=$1
     shift
     arr=("$@")
     local counter=0
-    for i in "${arr[@]}";
-        do
-            echo -n "waiting for pod $i in namespace $namespace..." >&2
-            while ! ("${KUBECTL}" -n $namespace get pod $i) &>/dev/null; do
-                if [ "$counter" -ge "$timeout" ]; then echo "TIMEOUT"; exit -1; else (( counter+=5 )); fi
-                echo -n "." >&2
-                sleep 5
-            done
-            echo "FOUND POD!" >&2
+    for i in "${arr[@]}"; do
+        echo -n "waiting for pod $i in namespace $namespace..." >&2
+        while ! ("${KUBECTL}" -n $namespace get pod $i) &>/dev/null; do
+            if [ "$counter" -ge "$timeout" ]; then
+                echo "TIMEOUT"
+                exit -1
+            else ((counter += 5)); fi
+            echo -n "." >&2
+            sleep 5
         done
+        echo "FOUND POD!" >&2
+    done
 }
 
-check_deployments(){
+check_deployments() {
     for name in $1; do
         echo_sub_step "inspecting deployment '${name}'"
         local dep_stat=$("${KUBECTL}" -n "$2" get deployments/"${name}")
@@ -67,7 +69,7 @@ check_deployments(){
         fi
 
         echo_info "check if is ready"
-        IFS='/' read -ra ready_status_parts <<< "$(echo "$dep_stat" | awk ' FNR > 1 {print $2}')"
+        IFS='/' read -ra ready_status_parts <<<"$(echo "$dep_stat" | awk ' FNR > 1 {print $2}')"
         if (("${ready_status_parts[0]}" < "${ready_status_parts[1]}")); then
             echo "is not Ready"
             exit -1
@@ -78,10 +80,10 @@ check_deployments(){
     done
 }
 
-check_pods(){
+check_pods() {
     pods=$("${KUBECTL}" -n "${CROSSPLANE_NAMESPACE}" get pods)
     count=$(echo "$pods" | wc -l)
-    if (("${count}"-1 != "${1}")); then
+    if (("${count}" - 1 != "${1}")); then
         sleep 10
         "${KUBECTL}" get events -A
         sleep 20
@@ -102,7 +104,7 @@ check_pods(){
         fi
 
         echo_info "check if is ready"
-        IFS='/' read -ra ready_status_parts <<< "$(echo "$pod_stat" | awk '{print $2}')"
+        IFS='/' read -ra ready_status_parts <<<"$(echo "$pod_stat" | awk '{print $2}')"
         if (("${ready_status_parts[0]}" < "${ready_status_parts[1]}")); then
             echo_error "is not ready"
             exit -1
@@ -119,18 +121,18 @@ check_pods(){
         fi
 
         echo_info "check if has restarts"
-        if (( $(echo "$pod_stat" | awk '{print $4}') > 0 )); then
+        if (($(echo "$pod_stat" | awk '{print $4}') > 0)); then
             echo_error "has restarts"
             exit -1
         else
             echo_step_completed
         fi
         echo
-    done <<< "$(echo "$pods" | awk 'FNR>1')"
+    done <<<"$(echo "$pods" | awk 'FNR>1')"
 }
 
 # ------------------------------
-projectdir="$( cd "$( dirname "${BASH_SOURCE[0]}")"/../.. && pwd )"
+projectdir="$(cd "$(dirname "${BASH_SOURCE[0]}")"/../.. && pwd)"
 
 # get the build environment variables from the special build.vars target in the main makefile
 eval $(make --no-print-directory -C ${projectdir} build.vars)
@@ -144,20 +146,20 @@ CONTROLLER_IMAGE="${BUILD_REGISTRY}/${PROJECT_NAME}-controller-${HOSTARCH}"
 version_tag="$(cat ${projectdir}/_output/version)"
 # tag as latest version to load into kind cluster
 PACKAGE_CONTROLLER_IMAGE="${DOCKER_REGISTRY}/${PROJECT_NAME}-controller:${VERSION}"
-K8S_CLUSTER="${K8S_CLUSTER:-${BUILD_REGISTRY}-INTTESTS}"
+K8S_CLUSTER="${K8S_CLUSTER:-${BUILD_REGISTRY}-inttests}"
 
 CROSSPLANE_NAMESPACE="crossplane-system"
 PACKAGE_NAME="provider-gcp"
 
 # cleanup on exit
 if [ "$skipcleanup" != true ]; then
-  function cleanup {
-    echo_step "Cleaning up..."
-    export KUBECONFIG=
-    "${KIND}" delete cluster --name="${K8S_CLUSTER}"
-  }
+    function cleanup {
+        echo_step "Cleaning up..."
+        export KUBECONFIG=
+        "${KIND}" delete cluster --name="${K8S_CLUSTER}"
+    }
 
-  trap cleanup EXIT
+    trap cleanup EXIT
 fi
 
 # setup package cache
@@ -169,7 +171,8 @@ docker save "${BUILD_IMAGE}" -o "${CACHE_PATH}/${PACKAGE_NAME}.xpkg" && chmod 64
 
 # create kind cluster with extra mounts
 echo_step "creating k8s cluster using kind"
-KIND_CONFIG="$( cat <<EOF
+KIND_CONFIG="$(
+    cat <<EOF
 kind: Cluster
 apiVersion: kind.x-k8s.io/v1alpha4
 nodes:
@@ -199,7 +202,8 @@ echo_step "create crossplane-system namespace"
 "${KUBECTL}" create ns crossplane-system
 
 echo_step "create persistent volume and claim for mounting package-cache"
-PV_YAML="$( cat <<EOF
+PV_YAML="$(
+    cat <<EOF
 apiVersion: v1
 kind: PersistentVolume
 metadata:
@@ -218,7 +222,8 @@ EOF
 )"
 echo "${PV_YAML}" | "${KUBECTL}" create -f -
 
-PVC_YAML="$( cat <<EOF
+PVC_YAML="$(
+    cat <<EOF
 apiVersion: v1
 kind: PersistentVolumeClaim
 metadata:
@@ -270,7 +275,8 @@ check_pods 2
 # install package
 echo_step "installing ${PROJECT_NAME} into \"${CROSSPLANE_NAMESPACE}\" namespace"
 
-INSTALL_YAML="$( cat <<EOF
+INSTALL_YAML="$(
+    cat <<EOF
 apiVersion: pkg.crossplane.io/v1beta1
 kind: Provider
 metadata:
